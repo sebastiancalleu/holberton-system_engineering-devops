8641b2dda2ecd6aa39f7bcb6f3314372341f3ed5
sebastian <sebastian.calleu@gmail.com>
2021-05-03 16:24:13 -0500

    adding main

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,22 +4,23 @@
 import requests
 import sys
 
-todos = requests.get("https://jsonplaceholder.typicode.com/todos").json()
-users = requests.get("https://jsonplaceholder.typicode.com/users").json()
+if __name__ == "__main__":
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+    users = requests.get("https://jsonplaceholder.typicode.com/users").json()
 
-number_of_done_tasks = 0
-total_number_of_tasks = 0
-tasks = ""
-for i in todos:
-    if i.get("userId") == int(sys.argv[1]):
-        total_number_of_tasks += 1
-        if i.get("completed") == True:
-            number_of_done_tasks += 1
-            tasks += "\t " + i.get("title") + "\n"
+    number_of_done_tasks = 0
+    total_number_of_tasks = 0
+    tasks = ""
+    for i in todos:
+        if i.get("userId") == int(sys.argv[1]):
+            total_number_of_tasks += 1
+            if i.get("completed") == True:
+                number_of_done_tasks += 1
+                tasks += "\t " + i.get("title") + "\n"
 
-for i in users:
-    if i.get("id") == int(sys.argv[1]):
-        employee_name = i.get("name")
+    for i in users:
+        if i.get("id") == int(sys.argv[1]):
+            employee_name = i.get("name")
 
-print("Employee {} is done with tasks({}/{}):".format(employee_name, number_of_done_tasks, total_number_of_tasks))
-print(tasks, end="")
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, number_of_done_tasks, total_number_of_tasks))
+    print(tasks, end="")
