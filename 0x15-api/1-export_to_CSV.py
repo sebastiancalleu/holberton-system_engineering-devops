db2d68becba4f46f1894298dde7068c4fba8770a
sebastian <sebastian.calleu@gmail.com>
2021-05-03 23:21:53 -0500

    making a filename variable

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,11 +9,12 @@ if __name__ == "__main__":
     todos = requests.get("https://jsonplaceholder.typicode.com/todos").json()
     users = requests.get("https://jsonplaceholder.typicode.com/users").json()
 
+    filename = "{}.csv".format(sys.argv[1])
     for i in users:
         if i.get("id") == int(sys.argv[1]):
             employee_name = i.get("name")
 
-    with open('{}.csv'.format(sys.argv[1]), 'w') as file:
+    with open(filename, 'w') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for i in todos:
             if i.get("userId") == int(sys.argv[1]):
