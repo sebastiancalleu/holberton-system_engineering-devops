7f5786bf20d07ae195112a0bef8b08e854d086af
sebastian <sebastian.calleu@gmail.com>
2021-05-03 23:09:19 -0500

    adding file for first time.

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" script to fetch data from an APIs """
+
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+    users = requests.get("https://jsonplaceholder.typicode.com/users").json()
+
+    for i in users:
+        if i.get("id") == int(sys.argv[1]):
+            employee_name = i.get("name")
+
+with open('USER_ID.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    for i in todos:
+        if i.get("userId") == int(sys.argv[1]):
+            writer.writerow([sys.argv[1], employee_name,
+                             i.get("completed"), i.get("title")])
