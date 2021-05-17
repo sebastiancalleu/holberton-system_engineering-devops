02c0d0577d9525fee4293f0bb1bb095459c8bb0a
sebastian <sebastian.calleu@gmail.com>
2021-05-17 17:14:25 -0500

    adding files for first time

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" module to get info of a subreddit """
+
+import requests
+import sys
+
+
+def top_ten(subreddit):
+    """ function to get the subscribers of a subreddit """
+    user = {
+        "User-Agent": "user/0.0.2"
+    }
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    data = requests.get(url, headers=user)
+    if data.status_code == 200:
+        dct = data.json()
+        posts = dct["data"]["children"][:10]
+        for post in posts:
+            print(post["data"]["title"])
+        return(dct)
+    return(0)
